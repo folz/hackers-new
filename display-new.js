ed741027031158c3c35f431044dc545fc4bed68d
Rodney Folz <rodney@rodneyfolz.com>
2016-11-13 13:24:20 -0800

    Render and update new stories on the front page.

--- display-new.js
+++ display-new.js
@@ -2,15 +2,93 @@ if (typeof browser === 'undefined') {
   var browser = chrome;
 }
 
+/* The table element containing stories on the front page */
+const FRONT_PAGE_ITEMS = document.querySelectorAll('#hnmain table.itemlist')[0];
+
+const RE_WWW_AT_START = /^(www\.)/;
+
+
+/**
+ * Returns the "site" of a string URL.
+ *
+ * As far as I can tell, HN considers this the hostname minus any
+ * leading "www".
+ *
+ * E.g.,
+ * > hostname("https://www.example.com/path?query=data#anchor")
+ * "example.com"
+ */
+function site(url) {
+  return new URL(url).hostname.replace(RE_WWW_AT_START,"");
+}
+
+
+/**
+ * Builds the item DOM elements to be inserted on the front page.
+ */
+function createItemDom(item) {
+  let htmlString =
+    `<tr class="athing" id="${item.id}">
+       <td align="right" valign="top" class="title">
+         <span class=rank">...</span>
+       </td>
+       <td valign="top" class="votelinks">
+         <center>
+           <a id="up_${item.id}" href="vote?id=${item.id}&how=up&goto=news">
+             <div class="votearrow" title="upvote"></div>
+           </a>
+         </center>
+       </td>
+       <td class="title">
+         ${item.url
+           ?
+           `<a href="${item.url}" class="storylink">${item.title}</a>
+            <span class="sitebit comhead">
+              (<a href="from?site=${site(item.url)}"><span class="sitestr">${site(item.url)}</span></a>)
+            </span>`
+           :
+           `<a href="item?id=${item.id}" class="storylink">${item.title}</a>`
+          }
+       </td>
+     </tr>
+     <tr>
+      <td colspan="2"></td>
+      <td class="subtext">
+        <span class="score" id="score_${item.id}">${item.score} point${(item.score === 1) ? "" : "s"}</span> by <a href="user?id=${item.by}" class="hnuser">${item.by}</a>
+        <span class="age">
+          <a href="item?id=${item.id}">on /newest</a>
+        </span>
+        <span id="unv_${item.id}"></span> | <a href="hide?id=${item.id}&goto=news">hide</a> | <a href="item?id=${item.id}">${item.descendants} comment${(item.descendants == 1) ? "" : "s"}</a>
+      </td>
+     </tr>
+     <tr class="spacer" style="height:5px"></tr>`;
+
+  let template = document.createElement('template');
+  template.innerHTML = htmlString;
+  return template.content.childNodes;
+}
+
 // We use the (key, callback) signature here instead of the promise
 // signature because Firefox supports both but Chrome only supports
 // the former.
-browser.storage.local.get('newstories', (store) => {
+browser.storage.local.get('storyIdsToDisplay', (store) => {
   // For some reason, Firefox returns a 1-length array containing the
   // store object, while Chrome returns the store object directly.
   if (Array.isArray(store)) {
     store = store[0];
   }
 
-  console.log(store.newstories);
+  // If there aren't any stories to display, default to an empty array
+  // so the .forEach doesn't error.
+  storyIdsToDisplay = store.storyIdsToDisplay || [];
+
+  browser.storage.local.get(storyIdsToDisplay, (store) => {
+    storyIdsToDisplay.forEach((id) => {
+      let itemDom = createItemDom(store[id]);
+
+      FRONT_PAGE_ITEMS.appendChild(itemDom[0]);
+      FRONT_PAGE_ITEMS.appendChild(itemDom[1]);
+      FRONT_PAGE_ITEMS.appendChild(itemDom[2]);
+    });
+  });
 });
--- refresh-new.js
+++ refresh-new.js
@@ -2,11 +2,30 @@ if (typeof browser === 'undefined') {
   var browser = chrome;
 }
 
-/* The number of stories on /newest to fetch each time. */
+/* The number of new stories to display on the front page. */
+const NEW_STORIES_DISPLAY_COUNT = 5;
+
+/* The number of stories on /newest to fetch each update. */
 const NEW_STORIES_FETCH_COUNT = 30;
 
-/* How often, in minutes, new stories are updated */
-const NEW_STORIES_UPDATE_INTERVAL = 15.0;
+/* How often, in minutes, new stories are fetched. */
+const NEW_STORIES_FETCH_INTERVAL = 15.0;
+
+/* How often, in minutes, displayed new stories are changed */
+const NEW_STORIES_UPDATE_INTERVAL = 5.0;
+
+
+/**
+ * In-place array shuffle using the Knuth algorithm.
+ */
+function shuffle(array) {
+    for (let i = array.length; i > 0; i--) {
+        let j = Math.floor(Math.random() * i);
+        [array[i - 1], array[j]] = [array[j], array[i - 1]]
+    }
+    return array;
+}
+
 
 /**
  * Grabs new HN stories and persists them in the extension's storage.
@@ -43,22 +62,63 @@ function fetchAndStoreNew() {
               return acc;
             }, {});
             browser.storage.local.set(storyItems);
+
+            updateDisplayedStories();
           });
-        })
-        .catch((err) => {
-          console.error(err)
         });
-    })
-    .catch((err) => {
-      console.error(err);
     });
 }
 
-browser.alarms.create("newstories", {
+function updateDisplayedStories() {
+  // We use the (key, callback) signature here instead of the promise
+  // signature because Firefox supports both but Chrome only supports
+  // the former.
+  browser.storage.local.get(['newstories', 'lastUpdate'], (store) => {
+    // For some reason, Firefox returns a 1-length array containing the
+    // store object, while Chrome returns the store object directly.
+    if (Array.isArray(store)) {
+      store = store[0];
+    }
+
+    // Because fetchAndStoreNew can call updateDisplayedStories, this
+    // check ensures we don't have two refreshes in quick succession.
+    // 60000 is the minutes-to-milliseconds constant.
+    const lastUpdate = new Date(store.lastUpdate || 0);
+    const delta = Date.now() - lastUpdate.getTime();
+    if (delta < NEW_STORIES_UPDATE_INTERVAL * 60000) {
+      return;
+    }
+
+    // browser.storage requires that keys are already strings. It doesn't
+    // implicitly convert integers.
+    let storyIdsToDisplay = shuffle(store.newstories)
+                              .slice(0, NEW_STORIES_DISPLAY_COUNT)
+                              .map(String);
+    browser.storage.local.set({
+      'storyIdsToDisplay': storyIdsToDisplay,
+      'lastUpdate': Date.now()
+    });
+  });
+
+}
+
+browser.alarms.create("fetchAndStoreNew", {
   'when': Date.now(),
-  'periodInMinutes': NEW_STORIES_UPDATE_INTERVAL
+  'periodInMinutes': NEW_STORIES_FETCH_INTERVAL
 });
 
+browser.alarms.create("updateDisplayedStories", {
+  'when': Date.now(),
+  'periodInMinutes': NEW_STORIES_UPDATE_INTERVAL
+})
+
 chrome.alarms.onAlarm.addListener((alarm) => {
-  fetchAndStoreNew();
+  switch (alarm.name) {
+    case "fetchAndStoreNew":
+      fetchAndStoreNew();
+      break;
+    case "updateDisplayedStories":
+      updateDisplayedStories();
+      break;
+  }
 });
